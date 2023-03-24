231973262d6a5cad45c5e43606aa0d96f2df9d04
Jignesh Khokhani <khokhanijignesh29@gmail.com>
2023-03-24 16:52:56 +0900

    Implement adding comment module

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates_presence_of :title, :body
 end
